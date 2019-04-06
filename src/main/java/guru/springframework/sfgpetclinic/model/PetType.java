32ebba6de0ef54f24254ac0e077adcfba1880010
saul <saul_harry_@hotmail.com>
2019-04-05 22:31:42 -0600

    agregando POJOs. Cierra #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public PetType() {
+    }
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PetType{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
