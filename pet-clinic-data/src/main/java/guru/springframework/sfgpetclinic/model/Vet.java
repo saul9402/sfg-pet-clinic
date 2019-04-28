85f7db356f10003e10cd870ce206bfa3a006198b
saul <saul_harry_@hotmail.com>
2019-04-27 23:21:45 -0500

    se agregarón propiedades al dueño. closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class Vet extends Person {
 
     private Set<Speciality> specialities;
-    
+
     public Set<Speciality> getSpecialities() {
         return specialities;
     }
