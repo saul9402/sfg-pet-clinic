54c500b028c06afc11624c54698e5e8a3b5bad26
saul <saul_harry_@hotmail.com>
2019-05-09 23:26:59 -0500

    Se agregan mascotas e informacion de los dueños al iniciar la aplicacion. closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 public class Pet extends BaseEntity {
 
+    private String name;
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
@@ -12,12 +13,19 @@ public class Pet extends BaseEntity {
     public Pet() {
     }
 
-    public Pet(PetType petType, Owner owner, LocalDate birthDate) {
+    public Pet(String name, PetType petType, Owner owner, LocalDate birthDate) {
+        this.name = name;
         this.petType = petType;
         this.owner = owner;
         this.birthDate = birthDate;
     }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     public PetType getPetType() {
         return petType;
     }
