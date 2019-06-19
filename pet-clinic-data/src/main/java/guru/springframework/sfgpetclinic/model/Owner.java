54766664012c98fad0b050c2b8c7bd24fed20908
saul <saul_harry_@hotmail.com>
2019-06-19 00:02:40 -0500

    se define el mapeo para las clases owner, pet y petType

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,42 +3,59 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
-    private String address;
-    private String city;
-    private String telephone;
-    private Set<Pet> pets = new HashSet<>();
+	private static final long serialVersionUID = 1L;
 
-    public String getAddress() {
-        return address;
-    }
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "city")
+	private String city;
+	
+	@Column(name = "telephone")
+	private String telephone;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	private Set<Pet> pets = new HashSet<>();
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public String getTelephone() {
-        return telephone;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
+	public String getTelephone() {
+		return telephone;
+	}
 
-    public Set<Pet> getPets() {
-        return pets;
-    }
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Column(name = "name")
 	private String name;
 
     public PetType() {
